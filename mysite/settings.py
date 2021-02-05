d6556395db0335d90b3ddb7b352160faa7bab7bc
Jongwan Lee <jjmean2@gmail.com>
2021-02-05 10:39:17 +0900

    Add BASE_DIR for templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['mysite/templates'],
+        'DIRS': [BASE_DIR / 'mysite/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
